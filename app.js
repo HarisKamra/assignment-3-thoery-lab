adb152932f54244d343bb4666024d46712450a9b
HarisKamra <114874241+HarisKamra@users.noreply.github.com>
2022-12-20 10:57:42 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from "@react-navigation/native";
+import Register from './app/components/Register';
+import Home from './app/components/Home';
+import Login from './app/components/Login';
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+const styles = StyleSheet.create({
+});
+
+
+
